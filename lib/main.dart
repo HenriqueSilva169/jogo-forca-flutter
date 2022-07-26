88d087fc2aba5576014a096b00bf568b234f6632
Henrique Gleison Silva <henriquedejijoca@gmail.com>
2022-07-26 16:15:40 -0300

    Adicionando verificador de letra

--- lib/main.dart
+++ lib/main.dart
@@ -10,19 +10,33 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List words = ["SUSHI", "BETERRABA", "MÃOS", "MANGA", "GUATEMALA"];
-    List dicas = [
+    List tips = [
       "COMIDA JAPONESA",
       "HORTALIÇA",
       "MEMBRO DO CORPO",
       "FRUTA",
       "PAÍS"
     ];
+    String tip = tips[randomNumber];
+    String word = words[randomNumber];
+    List foundLetters = [];
+
+    LetterVerifier(String letter) {
+      int x = 0;
+      word.runes.forEach((int rune) {
+        String character = new String.fromCharCode(rune);
+        print(character);
+        x++;
+      });
+    }
+
+    LetterVerifier("a");
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Text(words[randomNumber]),
+      home: Text(word),
     );
   }
 }
